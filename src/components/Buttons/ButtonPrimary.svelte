981dde4b93ed980fe61d6d960c9d9b3543e85df5
Jonathon Rivers <jdr2045@gmail.com>
2023-12-20 20:26:50 -0500

    Prisma init

---
+++ src/components/Buttons/ButtonPrimary.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let onClick: () => void;
+</script>
+
+<div>
+	<button
+		class="m-1 rounded-3xl bg-sky-400 px-4 py-2 font-bold text-white shadow-md shadow-gray-500"
+		on:click={onClick}><slot /></button
+	>
+</div>
